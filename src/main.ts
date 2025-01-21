746e5adbb60cec6cdfa03cc2570a044a3fd1c92d
WahabBello <abdouwahab123@gmail.com>
2025-01-22 00:55:27 +0100

    Configurer main ts :
    - router
    - vuetify
    - VueApexCharts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
-
+import router from './router';
+import vuetify from './plugins/vuetify';
+import VueApexCharts from "vue3-apexcharts";
 // Components
 import App from './App.vue'
 
@@ -14,7 +16,11 @@ import App from './App.vue'
 import { createApp } from 'vue'
 
 const app = createApp(App)
+app.use(router);
+app.use(vuetify);
+app.use(VueApexCharts);
 
+app.component("ApexChart", VueApexCharts);
 registerPlugins(app)
 
 app.mount('#app')
