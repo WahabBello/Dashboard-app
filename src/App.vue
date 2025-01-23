528d6a414da4ccf5c77e9cfcf34bcbe574a088e2
WahabBello <abdouwahab123@gmail.com>
2025-01-24 00:42:33 +0100

    Ajout de la fonctionnalité de changement de thème et configuration des thèmes clair et sombre

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,34 @@
     <v-app-bar app>
       <v-toolbar-title>Dashboard</v-toolbar-title>
       <v-spacer />
-      <v-btn to="/">
-        Home
-      </v-btn>
-      <v-btn to="/charts">
-        Charts
+      <!-- Bouton pour changer de thème -->
+      <v-spacer />
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/charts">Charts</v-btn>
+      <v-btn @click="toggleTheme" icon>
+        <v-icon>{{ currentThemeIcon }}</v-icon>
       </v-btn>
     </v-app-bar>
     <router-view />
   </v-app>
 </template>
 
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useTheme } from 'vuetify';
 
-<script lang="ts" setup>
-  //
+const theme = useTheme();
+const isDarkTheme = ref(theme.global.current.value.dark);
+
+// Fonction pour basculer le thème
+const toggleTheme = () => {
+  const newTheme = isDarkTheme.value ? 'light' : 'dark';
+  theme.global.name.value = newTheme;
+  isDarkTheme.value = !isDarkTheme.value;
+};
+
+// Icone correspondant au thème actuel
+const currentThemeIcon = computed(() =>
+  isDarkTheme.value ? 'mdi-weather-night' : 'mdi-white-balance-sunny'
+);
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,15 +5,49 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 // Composables
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#1976D2', // Bleu clair
+          secondary: '#424242', // Gris
+          accent: '#82B1FF', // Bleu clair accentué
+          error: '#FF5252', // Rouge
+          info: '#2196F3', // Bleu
+          success: '#4CAF50', // Vert
+          warning: '#FB8C00', // Orange
+        },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          primary: '#90CAF9', // Bleu clair
+          secondary: '#121212', // Gris foncé
+          accent: '#FF4081', // Rose accentué
+          error: '#FF5252', // Rouge
+          info: '#2196F3', // Bleu
+          success: '#4CAF50', // Vert
+          warning: '#FB8C00', // Orange
+        },
+      },
+    },
   },
-})
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
